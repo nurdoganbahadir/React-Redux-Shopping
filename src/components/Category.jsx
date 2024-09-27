1bc6b894eee84dc36d4e75020337a26a3b8bd11f
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-27 12:55:44 +0300

    category botunları düzenlendi

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,20 +4,9 @@ import React from "react";
 
 const Category = ({ category }) => {
   return (
-    <ButtonGroup
-      disableElevation
-      variant="contained"
-      aria-label="Disabled button group"
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        gap: 1, // Butonlar arası boşluk
-        my: 2, // Üstten boşluk
-        borderRadius: 2, // Buton köşelerinin yumuşatılması
-      }}
-    >
+    
       <Button color="secondary">{category}</Button>
-    </ButtonGroup>
+    
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,12 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setCategory, setProducts } from "../redux/action/productAction";
 import { useSelector } from "react-redux";
-import { Button, Grid2 } from "@mui/material";
+import { Button, ButtonGroup, Grid2 } from "@mui/material";
 
 const Home = () => {
   const { products } = useSelector((state) => state.productReducer);
   const { category } = useSelector((state) => state.categoryReducer);
-  console.log(category);
+
   const dispatch = useDispatch();
 
   const getApi = async () => {
@@ -32,10 +32,23 @@ const Home = () => {
 
   return (
     <section>
-      <Button> All</Button>
-      {category?.map((category) => (
-        <Category category={category} />
-      ))}
+      <ButtonGroup
+        disableElevation
+        variant="contained"
+        aria-label="Disabled button group"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          gap: 1, // Butonlar arası boşluk
+          my: 2, // Üstten boşluk
+          borderRadius: 2, // Buton köşelerinin yumuşatılması
+        }}
+      >
+        <Button> All</Button>
+        {category?.map((category) => (
+          <Category category={category} />
+        ))}
+      </ButtonGroup>
 
       <Grid2 container spacing={2}>
         {products?.map((product) => (
